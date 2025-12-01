61c146d022758b1f02654d414424075a481b9015
Sofiane Cheriette <149703512+sofiane-cheriette@users.noreply.github.com>
2025-12-01 10:24:19 +0100

    Implement basic arithmetic and logical operations

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -1,13 +1,10 @@
-#ifndef OPERATOR_H
-#define OPERATOR_H
+#include "operator.h"
 
-int somme(int num1, int num2);
-int difference(int num1, int num2);
-int produit(int num1, int num2);
-int quotient(int num1, int num2);
-int modulo(int num1, int num2);
-int et_logique(int num1, int num2);
-int ou_logique(int num1, int num2);
-int negation(int num1);
-
-#endif
+int somme(int num1, int num2) { return num1 + num2; }
+int difference(int num1, int num2) { return num1 - num2; }
+int produit(int num1, int num2) { return num1 * num2; }
+int quotient(int num1, int num2) { return (num2 != 0) ? num1 / num2 : 0; }
+int modulo(int num1, int num2) { return (num2 != 0) ? num1 % num2 : 0; }
+int et_logique(int num1, int num2) { return num1 & num2; }
+int ou_logique(int num1, int num2) { return num1 | num2; }
+int negation(int num1) { return ~num1; }
